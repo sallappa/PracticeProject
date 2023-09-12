1b3b6f978c16c3646ca6be54383ded48e78c3d11
sallappa <144332503+sallappa@users.noreply.github.com>
2023-09-12 18:21:52 +0530

    Update constructors.java

--- src/practiceProjects/constructors.java
+++ src/practiceProjects/constructors.java
@@ -13,6 +13,14 @@ public class constructors {
 		System.out.println("Parameterized constructor: " + age);
 
 	}
+	//same parameters
+	public constructors(int age,String name) {
+		System.out.println("same paarameters but age,name but order different");
+	}
+	//different parameters
+	public constructors(String name,int age,int id) {
+		System.out.println("with different parameters");
+	}
 
 	public static void main(String[] args) {
 
@@ -21,6 +29,10 @@ public class constructors {
 
 		// Creating an object using the parameterized constructor
 		constructors constructors2 = new constructors("sai",23);
+		//different parameters
+		constructors constructors3=new constructors("sai",23,405);
+		//same parameters but insertion order is different 
+		constructors constructors4=new constructors(23,"sai");
 
 	}
 }
